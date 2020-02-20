45ae4d43bf7bb5158b5382f456cd3ef245c4edc8
Kellogg <mkellogg@axiosws-112a.lgsdirect.com>
2020-02-20 09:32:47 -0500

    cleanup

--- src/mongodb.js
+++ src/mongodb.js
@@ -7,32 +7,14 @@ module.exports = function (app) {
   const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => {
       client.db(database);
-      app.service('users').Model = client.db('NRPDD').collection('users')
+      app.service('users').Model = client.db('NRPDD').collection('users');
       
     })
     .catch(error => {
-      console.log('promise error: ', error)
+      console.log('promise error: ', error);
     })
-  //app.set('mongoClient', mongoClient);
+  
   
 };
 
-
-// module.exports = function (app) {
-//   const MongoClient = require('mongodb').MongoClient;
-//   const uri = "mongodb+srv://user:altec1991@cluster0-a1j7v.mongodb.net/test?retryWrites=true&w=majority";
-//   const client = new MongoClient(uri, { useNewUrlParser: true });
-//   client.connect(err => {
-//     const collection = client.db("NRPDD").collection("users");
-//     // perform actions on the collection object
-
-//     // var myObj = {email: 'testuser2@test.com', password: 'mysecretpassycode2'};
-
-//     // collection.insertOne(myObj, (err, res) =>{
-//     //   if (err) throw err;
-//     //   console.log('here is res ', res)
-//     // })
-//     app.set('mongoClient', client)
-//   });
-// }
 
